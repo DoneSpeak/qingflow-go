ce0cc33fb58b9c25f06e3ecbad6a803ead8381a5
guanrong.yang <yangguanrong@9realms.co>
2023-06-18 01:38:52 +0800

    add role api

--- role_api.go
+++ role_api.go
@@ -1,3 +1,114 @@
 package qingflowapi
 
-type RoleApi struct{}
+import (
+	"fmt"
+)
+
+type RoleApi struct {
+	client Client
+}
+
+type Role struct {
+	RoleId   ID
+	RoleName string
+	OptionId ID
+}
+
+func (api RoleApi) DeleteRoleUser(roleId ID, userIds []ID) (ID, error) {
+	endpoint := "role/%s/user"
+	request := map[string]any{
+		"roleId":  roleId,
+		"userIds": userIds,
+	}
+	var result ApiResponse[Role]
+	err := api.client.deleteRequest(endpoint, request, &result)
+	if err != nil {
+		return 0, err
+	}
+	return result.Result.RoleId, nil
+}
+
+func (api RoleApi) AddRoleUser(roleId ID, userIds []ID) (ID, error) {
+	endpoint := "role/%s/user"
+	request := map[string]any{
+		"roleId":  roleId,
+		"userIds": userIds,
+	}
+	var result ApiResponse[Role]
+	err := api.client.post(endpoint, request, &result)
+	if err != nil {
+		return 0, err
+	}
+	return result.Result.RoleId, nil
+}
+
+func (api RoleApi) DeleteRole(roleId ID) (ID, error) {
+	endpoint := fmt.Sprintf("role/%s", roleId)
+	var result ApiResponse[Role]
+	err := api.client.deleteRequest(endpoint, nil, &result)
+	if err != nil {
+		return 0, err
+	}
+	return result.Result.RoleId, nil
+}
+
+func (api RoleApi) Update(roleId ID, roleName string) (ID, error) {
+	endpoint := fmt.Sprintf("role/%s", roleId)
+	request := Role{RoleId: roleId, RoleName: roleName}
+	var result ApiResponse[Role]
+	err := api.client.post(endpoint, request, &result)
+	if err != nil {
+		return 0, err
+	}
+	return result.Result.RoleId, nil
+}
+
+func (api RoleApi) Create(roleId ID, roleName string) (ID, error) {
+	endpoint := fmt.Sprintf("role")
+	request := Role{RoleId: roleId, RoleName: roleName}
+	var result ApiResponse[Role]
+	err := api.client.post(endpoint, request, &result)
+	if err != nil {
+		return 0, err
+	}
+	return result.Result.RoleId, nil
+}
+
+func (api RoleApi) GetAll() ([]Role, error) {
+	endpoint := fmt.Sprintf("role")
+	var result ApiResponse[struct {
+		Role []Role `json:"role"`
+	}]
+	err := api.client.get(endpoint, nil, &result)
+	if err != nil {
+		return []Role{}, err
+	}
+	return result.Result.Role, nil
+}
+
+type User struct {
+	AreaCode         string `json:"areaCode"`
+	BeingActive      bool   `json:"beingAtive"`
+	BeingDisabled    bool   `json:"beingDisabled"`
+	CustomDepartment []ID   `json:"customDepartment"`
+	CustomRole       []ID   `json:"customRole"`
+	Department       []ID   `json:"department"`
+	Email            string `json:"email"`
+	HeadImg          string `json:"headImg"`
+	MobileNum        string `json:"mobileNum"`
+	Name             string `json:"name"`
+	Role             []ID   `json:"role"`
+	UserId           string `json:"userId"`
+}
+
+func (api RoleApi) GetRoleUser(roleId ID) ([]User, error) {
+	endpoint := fmt.Sprintf("role")
+	var result ApiResponse[struct {
+		UserList []User `json:"userList"`
+	}]
+	err := api.client.get(endpoint, nil, &result)
+	if err != nil {
+		return []User{}, err
+	}
+	return result.Result.UserList, nil
+}
